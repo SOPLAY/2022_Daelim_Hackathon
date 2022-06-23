ef6f941720a46b236efa67a77fd572fd87caffa9
Mallang-Mallang <gudwns1458@naver.com>
2022-06-23 20:17:25 +0900

    [ Client Side ] Fix Base Container Style

--- core/components/common/Container.tsx
+++ core/components/common/Container.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-const Container = () => {
-  return (
-    <div className="flex w-[100vw] h-[100vh] items-center justify-center">
-      <div className="w-[90vw] h-[90vh] max-w-[1440px] max-h-[1024px] bg-white/30 rounded-[50px] border backdrop-blur-md drop-shadow-xl z-10 p-5">
-        asd
-      </div>
-    </div>
-  );
-};
+const Container = ({ children }) => (
+  <div className="w-full h-full bg-white/30 rounded-[50px] border backdrop-blur-md drop-shadow-xl z-10 p-5">
+    {children}
+  </div>
+);
 
 export default Container;
