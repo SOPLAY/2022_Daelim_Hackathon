52b25bd9b7f726e4e8dd252cebae6f8d8f9cc20b
soplay <tkdgns1341@gmail.com>
2022-06-25 02:45:27 +0900

    [ ClientSide ] Fix Summery

--- core/components/Summery/index.tsx
+++ core/components/Summery/index.tsx
@@ -23,52 +23,52 @@ const index = () => {
   const totalVol = testData.reduce((acc, cur) => acc + cur.value, 0);
 
   return (
-    <div className='flex justify-center w-full h-full px-10 mt-10'>
-      <div className='flex w-1/2 h-5/6 mr-10'>
+    <div className="flex justify-center w-full h-full px-10">
+      <div className="flex w-1/2 h-[90%] mr-10">
         <Container>
-          <div className='flex flex-col mx-auto my-auto'>이미지</div>
+          <div className="flex flex-col mx-auto my-auto">이미지</div>
         </Container>
       </div>
-      <div className='flex  w-1/2 h-5/6'>
+      <div className="flex  w-1/2 h-[90%]">
         <Container>
-          <div className='flex flex-col h-full p-[30px]'>
-            <h2 className='drop-shadow-md ml-5'>Volumes</h2>
-            <div className='flex flex-col items-center'>
+          <div className="flex flex-col h-full p-[30px]">
+            <h2 className="drop-shadow-md ml-5">Volumes</h2>
+            <div className="flex flex-col items-center">
               {testData.map((v) => {
                 return (
                   <div
-                    className='h-[60px] mt-5 flex justify-between px-5 items-center w-[90%]  bg-white/30 rounded-[10px] backdrop-blur-md drop-shadow-xl z-10'
+                    className="h-[60px] mt-5 flex justify-between px-5 items-center w-[90%]  bg-white/30 rounded-[10px] backdrop-blur-md drop-shadow-xl z-10"
                     key={v.id}
                   >
-                    <h3 className='flex m-0'>{`${v.name}: ${v.value}`}</h3>
-                    <div className='flex'>
+                    <h3 className="flex m-0">{`${v.name}: ${v.value}`}</h3>
+                    <div className="flex">
                       <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        className='h-7 w-7 cursor-pointer text-gray-500 hover:text-black mr-5'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-7 w-7 cursor-pointer text-gray-500 hover:text-black mr-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
                         strokeWidth={2}
                       >
                         <path
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                          d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
                         />
                       </svg>
 
                       <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        className='h-7 w-7 cursor-pointer text-gray-500 hover:text-red-600'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-7 w-7 cursor-pointer text-gray-500 hover:text-red-600 "
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
                         strokeWidth={2}
                       >
                         <path
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                          d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16'
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                         />
                       </svg>
                     </div>
@@ -77,9 +77,9 @@ const index = () => {
               })}
             </div>
           </div>
-          <div className='flex flex-col justify-center items-center w-full h-1/3 border-white border-t-2'>
-            <div className='flex flex-col w-[80%] h-[60px] mt-5 items-start bg-white/30 rounded-[10px] border backdrop-blur-md drop-shadow-xl z-10'>
-              <h2 className='mx-auto'>Total Volume: {totalVol}</h2>
+          <div className="flex flex-col justify-center items-center w-full h-1/4 border-white border-t-2">
+            <div className="flex flex-col w-[80%]   items-start bg-white/30 rounded-[10px] border backdrop-blur-md drop-shadow-xl z-10">
+              <h2 className="mx-auto">Total Volume: {totalVol}</h2>
             </div>
           </div>
         </Container>
--- pages/summery.tsx
+++ pages/summery.tsx
@@ -4,53 +4,14 @@ import Summery from '@components/Summery';
 import Logo from '@components/common/Logo';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import Header from '@components/common/Header';
 
 const Home = () => {
   const router = useRouter();
   return (
-    <div className='w-[1440px] h-[1024px] flex justify-center content-center my-auto'>
+    <div className="w-[90%] h-[90%] w-max-[1440px] h-max-[1024px] flex justify-center content-center my-auto">
       <Container>
-        <div className='flex px-[45px] pt-[45px] mb-5'>
-          <Logo />
-          <div className='flex justify-end items-center w-[75%]'>
-            <Link href='/summery'>
-              <h3
-                className={`mr-10 hover:text-gray-500 cursor-pointer ${
-                  router.pathname === '/summery'
-                    ? 'text-gray-500'
-                    : 'text-black'
-                }`}
-              >
-                Summery
-              </h3>
-            </Link>
-            <Link href='/calender'>
-              <h3
-                className={`mr-10 hover:text-gray-500 cursor-pointer ${
-                  router.pathname === '/calender'
-                    ? 'text-gray-500'
-                    : 'text-black'
-                }`}
-              >
-                Calendar
-              </h3>
-            </Link>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-8 w-8 mb-3 cursor-pointer'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1'
-              />
-            </svg>
-          </div>
-        </div>
+        <Header />
         <Summery />
       </Container>
     </div>
