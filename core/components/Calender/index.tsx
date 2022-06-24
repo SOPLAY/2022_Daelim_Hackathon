aff49d8e52de3375c0deaead0aa5d24a592b04f3
soplay <tkdgns1341@gmail.com>
2022-06-25 04:59:22 +0900

    [ ClientSide ] Fix Calender components

--- core/components/Calender/index.tsx
+++ core/components/Calender/index.tsx
@@ -4,6 +4,7 @@ import { Calendar, Col, Radio, Row, Select, Typography } from 'antd';
 import Container from '@components/common/Container';
 import InformationCard from './InformationCard';
 import api from '@lib/api';
+import Loading from '@components/common/Loading';
 
 const Calender = () => {
   const onPanelChange = (value, mode) => {
@@ -11,8 +12,12 @@ const Calender = () => {
   };
 
   const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [first, setFirst] = useState(true);
 
   const onClick = async (date) => {
+    setLoading(true);
+
     await api.user
       .get({ date: new Date(date).toLocaleDateString() })
       .then((res) => {
@@ -22,17 +27,26 @@ const Calender = () => {
       .catch((e) => {
         console.log(e);
       });
+    setLoading(false);
+    setFirst(false);
   };
 
+  useEffect(() => {
+    first && !data.length && onClick(new Date());
+  });
+
   return (
-    <div className='flex justify-center w-full h-full px-10 mt-10'>
-      <div className='flex w-1/2 h-5/6 mr-10'>
+    <div className="flex justify-center w-full h-[90%] px-10">
+      <div className="flex w-1/2 h-[90%] mr-10">
         <Container>
-          <div className='flex justify-center items-center'>
-            <div className='flex justify-center items-center h-[680px] w-[530px]'>
-              <div className='h-[90%] relative'>
+          <div className="flex justify-center items-center h-full w-full ">
+            <div className="flex justify-center items-center  w-[400px] ">
+              <div className="h-[90%] relative">
                 <Calendar
-                  onChange={(data) => onClick(data)}
+                  onChange={(data) => {
+                    setData([]);
+                    onClick(data);
+                  }}
                   fullscreen={true}
                   headerRender={({ value, type, onChange, onTypeChange }) => {
                     const start = 0;
@@ -48,7 +62,7 @@ const Calender = () => {
 
                     for (let index = start; index < end; index++) {
                       monthOptions.push(
-                        <Select.Option className='month-item' key={`${index}`}>
+                        <Select.Option className="month-item" key={`${index}`}>
                           {months[index]}
                         </Select.Option>
                       );
@@ -60,7 +74,7 @@ const Calender = () => {
 
                     for (let i = year - 10; i < year + 10; i += 1) {
                       options.push(
-                        <Select.Option key={i} value={i} className='year-item'>
+                        <Select.Option key={i} value={i} className="year-item">
                           {i}
                         </Select.Option>
                       );
@@ -78,13 +92,13 @@ const Calender = () => {
                           }
                           .ant-picker-calendar .ant-picker-panel {
                             background: none;
-                            font-size: 20px;
+                            font-size: 17px;
                             margin: 0;
                           }
                           .ant-picker-calendar-full
                             .ant-picker-panel
                             .ant-picker-calendar-date {
-                            height: 70px;
+                            height: 50px;
                             margind: 0;
                           }
                         `}</style>
@@ -94,19 +108,19 @@ const Calender = () => {
                         <Row gutter={8}>
                           <Col>
                             <Radio.Group
-                              size='small'
+                              size="small"
                               onChange={(e) => onTypeChange(e.target.value)}
                               value={type}
                             >
-                              <Radio.Button value='month'>Month</Radio.Button>
-                              <Radio.Button value='year'>Year</Radio.Button>
+                              <Radio.Button value="month">Month</Radio.Button>
+                              <Radio.Button value="year">Year</Radio.Button>
                             </Radio.Group>
                           </Col>
                           <Col>
                             <Select
-                              size='small'
+                              size="small"
                               dropdownMatchSelectWidth={false}
-                              className='my-year-select'
+                              className="my-year-select"
                               onChange={(newYear) => {
                                 const now = value.clone().year(Number(newYear));
                                 onChange(now);
@@ -118,7 +132,7 @@ const Calender = () => {
                           </Col>
                           <Col>
                             <Select
-                              size='small'
+                              size="small"
                               dropdownMatchSelectWidth={false}
                               value={String(month)}
                               onChange={(selectedMonth) => {
@@ -141,12 +155,20 @@ const Calender = () => {
           </div>
         </Container>
       </div>
-      <div className='w-[680px] h-[695px]'>
+      <div className="w-1/2 h-[90%]">
         <Container>
-          <div className='p-[40px]'>
+          <div className="px-[40px]  overflow-hidden box-border">
             <h3>More Information</h3>
-            <div className='flex flex-col items-center h-[530px] overflow-scroll'>
-              {data ? (
+            <div className="flex flex-col items-center h-[550px] overflow-scroll pb-14">
+              {isLoading ? (
+                <>
+                  <Loading type="calender" />
+                  <Loading type="calender" />
+                  <Loading type="calender" />
+                  <Loading type="calender" />
+                  <Loading type="calender" />
+                </>
+              ) : data.length ? (
                 data.map((v, i) => {
                   const { name, weight, count, set } = v;
                   console.log(name, weight, count, set);
@@ -156,13 +178,14 @@ const Calender = () => {
                       weight={weight}
                       count={count}
                       set={set}
+                      progress={10}
                       key={i}
                     />
                   );
                 })
               ) : (
                 <div>
-                  <h3>없음</h3>
+                  <h4>해당 날짜에 데이터가 존재하지 않습니다.</h4>
                 </div>
               )}
             </div>
