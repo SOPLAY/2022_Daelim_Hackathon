3b4587da4872431003edab4be5271e93be55ea8f
soplay <tkdgns1341@gmail.com>
2022-06-24 10:04:25 +0900

    [ ServerSide ] Add nextAuth SECRET KEY

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,17 @@
-import prisma from "@lib/prisma";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth from "next-auth";
-import KakaoProvider from "next-auth/providers/kakao";
+import prisma from '@lib/prisma';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import NextAuth from 'next-auth';
+import KakaoProvider from 'next-auth/providers/kakao';
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     KakaoProvider({
-      clientId: process.env.NEXTAUTH_KAKAO_KEY || "",
-      clientSecret: process.env.NEXTAUTH_KAKAO_SECRET_KEY || "",
+      clientId: process.env.NEXTAUTH_KAKAO_KEY || '',
+      clientSecret: process.env.NEXTAUTH_KAKAO_SECRET_KEY || '',
     }),
     // ...add more providers here
   ],
   adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
 });
