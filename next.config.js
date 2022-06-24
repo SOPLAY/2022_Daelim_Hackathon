fb5693004ce3223a9a7bf9f376702ddc47d0f33b
soplay <tkdgns1341@gmail.com>
2022-06-24 11:56:23 +0900

    [ ClientSide ] Fix webpack err

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.Push({
+  webpack: (config) => {
+    config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
       use: ['@svgr/webpack'],
     });
     return config;
