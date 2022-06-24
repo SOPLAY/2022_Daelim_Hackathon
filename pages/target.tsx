adc4b98fbbce9056e33af3397b66204e410a4b8c
soplay <tkdgns1341@gmail.com>
2022-06-25 02:03:13 +0900

    [ ClientSide ] Fix ajax logic error

--- pages/target.tsx
+++ pages/target.tsx
@@ -4,6 +4,7 @@ import BodyContainer from '../core/components/common/BodyContainer';
 import Logo from '../core/components/common/Logo';
 import { useRouter } from 'next/router';
 import api from '@lib/api';
+import Header from '@components/common/Header';
 
 const Home = () => {
   const [youtubeData, setYoutubeData] = useState([]);
@@ -42,39 +43,35 @@ const Home = () => {
           console.log(e.response);
         });
     };
-    getYoutubeData();
+    !youtubeData.length && getYoutubeData();
   }, []);
-  console.log(youtubeData);
+
   return (
-    <div className='w-[1440px] h-[1024px] flex justify-center content-center my-auto'>
+    <div className="w-[90%] h-[90%] w-max-[1440px] h-max-[1024px] flex justify-center content-center my-auto relative">
       <Container>
-        <div className='flex flex-col px-[45px] pt-[45px] mb-5'>
-          <Logo />
-          <h2 className='ml-[90px] leading-8'>{searchData}</h2>
-        </div>
-        <div className='flex w-full h-full px-10'>
-          <div className='flex  w-1/3 h-5/6 mr-10'>
+        <Header />
+        <div className="flex w-full h-[87%] px-10 ">
+          <div className="flex  w-1/3 h-[90%] mr-10 ">
             <Container>
-              <div className='flex flex-col mx-auto my-auto pl-5 pointer-events-none'>
-                <h3 className='ml-3'>Target</h3>
-                <BodyContainer width={400} type={'target'} />
+              <div className="flex flex-col mx-auto my-auto pl-5 pointer-events-none">
+                <BodyContainer width={'100%'} type={'target'} />
               </div>
             </Container>
           </div>
-          <div className='flex  w-2/3 h-5/6'>
+          <div className="flex w-2/3 h-[90%]">
             <Container>
-              <div className='flex flex-col p-10 h-full'>
+              <div className="flex flex-col p-10 h-full">
                 <h3>How to work?</h3>
-                <div className='flex flex-col h-full justify-between mt-5 overflow-scroll'>
+                <div className="flex flex-col h-full justify-between mt-5 overflow-scroll">
                   {youtubeData.map((e, i) => {
                     return (
-                      <div className='mb-5' key={i}>
+                      <div className="mb-5" key={i}>
                         <Container type={'target'}>
-                          <div className='flex p-5'>
+                          <div className="flex p-5">
                             <img
-                              className='cursor-pointer'
+                              className="cursor-pointer"
                               src={`${youtubeData[i].snippet.thumbnails.default.url}`}
-                              width='120px'
+                              width="120px"
                               onClick={() =>
                                 window.open(
                                   `https://www.youtube.com/watch?v=${youtubeData[i].id.videoId}`,
@@ -82,7 +79,7 @@ const Home = () => {
                                 )
                               }
                             />
-                            <div className='ml-5 cursor-pointer'>
+                            <div className="ml-5 cursor-pointer">
                               <h4
                                 onClick={() =>
                                   window.open(
@@ -97,7 +94,7 @@ const Home = () => {
                                   : youtubeData[i].snippet.title}
                               </h4>
                               <h5
-                                className='text-[#7c7c7c] mb-2 cursor-pointer'
+                                className="text-[#7c7c7c] mb-2 cursor-pointer"
                                 onClick={() =>
                                   window.open(
                                     `https://www.youtube.com/watch?v=${youtubeData[i].id.videoId}`,
