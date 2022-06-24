7eb9561b6d31a3fe8c5788912bd17a8702139c5c
Mallang-Mallang <gudwns1458@naver.com>
2022-06-24 11:12:07 +0900

    [ ClientSide ] Add SignIn page, Haeder

---
+++ pages/signin.tsx
@@ -0,0 +1,13 @@
+import SignIn from "../core/components/SignIn";
+import React from "react";
+import Container from "../core/components/common/Container";
+
+function Home() {
+  return (
+    <>
+      <SignIn />
+    </>
+  );
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./core/**/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "kakao-yellow": "#FEE500",
+      },
+    },
   },
   plugins: [],
 };
