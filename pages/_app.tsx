bde54a638e245209c7f8f17ea7c7540c18b8c600
soplay <tkdgns1341@gmail.com>
2022-06-25 12:19:00 +0900

    [ HOTFIX ] Add title teg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>MUSCLE.md</title>
+      </Head>
+
       <style jsx global>
         {`
           body {
